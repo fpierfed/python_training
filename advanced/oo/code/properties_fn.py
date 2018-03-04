60b9ec00375b0c73d28ebcf7c9768d7c303d81ce
Francesco Pierfederici <fpierfed@megabeets.com>
2018-03-04 19:32:30 +0100

    initial

---
+++ advanced/oo/code/properties_fn.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+from math import pi, sqrt
+
+
+Point = namedtuple('Point', ('x', 'y'))
+
+
+def type_check(name, required_type):
+    ivar_name = f'_{name}'
+
+    @property
+    def ivar(instance):
+        return getattr(instance, ivar_name)
+
+    @ivar.setter
+    def ivar(instance, value):
+        if not isinstance(value, required_type):
+            raise TypeError(f'expected {required_type.__name__}')
+        setattr(instance, ivar_name, value)
+
+    # return the ivar property so that it can be used by a class.
+    return ivar
+
+
+class Circle:
+    radius = type_check('radius', float)
+    center = type_check('center', Point)
+
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+
+    @property
+    def area(self):
+        return pi * self.radius ** 2
+
+    @area.setter
+    def area(self, value):
+        if value < 0:
+            # What???
+            raise ValueError('area cannot be negative')
+        self.radius = sqrt(value / pi)
