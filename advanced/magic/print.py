81936a08b175e4285abdec1991b2bb48891a8591
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-09 13:11:52 +0200

    initial

---
+++ advanced/magic/print.py
@@ -0,0 +1,10 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+if __name__ == '__main__':
+    p = Point(1, 2)
+    print(f'Users: this is my new point: {p}')
+    print(f'Developers: p = {p!r}')
