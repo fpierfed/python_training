bdcbff9e3902ebe57cadd3f0af19ebfa4ee2a602
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-11 18:10:26 +0200

    simplified code

--- async/code/coro1.py
+++ async/code/coro1.py
@@ -1,42 +1,15 @@
-"""
-Simple example of a generator function in pure Python.
-"""
+def foo():
+    print('Hello')
+    yield
+    print('World')
+    yield
+    print('Bye')
+    yield
+    print('World')
 
 
-def myrange(n):
-    assert n >= 0
-
-    i = 0
-    while i < n:
-        yield i
-        i += 1
-
-
-if __name__ == '__main__':
-    # for x in myrange(10):
-    #     print(x)
-    #
-    # print(list(myrange(10)))
-
-    # coro = myrange(10)
-    # print(coro)
-    # print(coro.send(None))
-    # print(next(coro))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-    # print(coro.send(None))
-
-    coro = myrange(10)
-    coro.send(None)
-
-    while True:
-        try:
-            print(coro.send(None))
-        except StopIteration:
-            break
+# g = foo()
+# next(g)
+# next(g)
+# next(g)
+# next(g)
--- async/code/coro2.py
+++ async/code/coro2.py
@@ -1,29 +1,18 @@
-"""
-Simple example of generator function in pure Python.
-"""
+def foo():
+    print('Hello')
+    yield
+    print('World')
+    yield
+    print('Bye')
+    yield
+    print('World')
+    return 42
 
 
-def say(msg='Hello, world'):
-    i = 0
-
-    while True:
-        print(f'[coroutine] Iteration {i}')
-
-        msg = yield msg
-        if msg is None:
-            break
-
-        print(f'[coroutine] Iteration {i} end')
-        i += 1
-
-
-if __name__ == '__main__':
-    coro = say()
-    print('[system] Calling send method')
-    print(coro.send(None))
-    print('[system] Calling send method')
-    print(coro.send('Foo!'))
-    print('[system] Calling send method')
-    print(coro.send('Bar!'))
-    print('[system] Calling send method')
-    print(coro.send(None))
+# g = foo()
+# while True:
+#     try:
+#         next(g)
+#     except StopIteration as e:
+#         print(f'Coroutine returned {e.value}')
+#         break
