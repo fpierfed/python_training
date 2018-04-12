ec32102b1b9b7b64a537658d788b0d0464a9367a
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-12 14:02:57 +0200

    bugfix: I was adding tasks multiple times

--- async/code/loop7.py
+++ async/code/loop7.py
@@ -75,14 +75,12 @@ class Loop:
         self._stop = False
 
     def create_task(self, coroutine):
-        if not isinstance(coroutine, Task):
-            task = Task(coroutine)
-        else:
-            task = coroutine
+        if isinstance(coroutine, Task):
+            return coroutine
 
-        if task not in self._tasks:
-            self._tasks.append(task)
-            print(f'Added Task {task.id}')
+        task = Task(coroutine)
+        self._tasks.append(task)
+        print(f'Added Task {task.id}')
         return task
 
     def remove(self, task):
