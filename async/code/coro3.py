24d77bd470a2edf292cb85a41d0c4a82857f789d
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-12 11:30:37 +0200

    initial

---
+++ async/code/coro3.py
@@ -0,0 +1,19 @@
+class Foo:
+    def __iter__(self):
+        print('Hello')
+        yield
+        print('World')
+        yield
+        print('Bye')
+        yield
+        print('World')
+        return 42
+
+
+# g = iter(Foo())
+# while True:
+#     try:
+#         next(g)
+#     except StopIteration as e:
+#         print(f'Coroutine returned {e.value}')
+#         break
