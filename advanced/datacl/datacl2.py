53d46eef2859b9af2221c8c51f6867622904c6e9
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-20 10:18:41 +0200

    simple renaming

--- advanced/datacl/datacl2.py
+++ advanced/datacl/datacl2.py
@@ -1,4 +1,20 @@
-from dataclasses import dataclass
+def dataclass(cls):
+    def __init__(self, *args):
+        for k, v in zip(cls.__annotations__.keys(), args):
+            setattr(self, k, v)
+        post_init = getattr(self, '__post_init__', None)
+        if post_init:
+            post_init()
+
+    def __repr__(self):
+        kwargs = {k: getattr(self, k) for k in cls.__annotations__}
+        kwstr = ', '.join([f'{k}={v}' for k, v in kwargs.items()])
+        return f'{cls.__name__}({kwstr})'
+
+    setattr(cls, '__init__', __init__)
+    setattr(cls, '__repr__', __repr__)
+    setattr(cls, '__str__', __repr__)
+    return cls
 
 
 @dataclass
