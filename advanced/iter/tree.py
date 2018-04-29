c2bb7012387e5e6c05a50b6e0517e7e234626d2c
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-29 16:01:30 +0200

    added option to exclude some dirs

--- advanced/iter/tree.py
+++ advanced/iter/tree.py
@@ -1,33 +1,40 @@
+import argparse
 import os
-import sys
 
 
 INDENT_STEP = '    '
 
 
-def print_tree(root, indent=''):
-    for line in _tree(root):
+def print_tree(root, exclude_dirs=None, indent=''):
+    for line in _tree(root, exclude_dirs, indent):
         print(line)
 
 
-def _tree(root, indent=''):
+def _tree(root, exclude_dirs=None, indent=''):
+    if exclude_dirs is None:
+        exclude_dirs = []
+
+    # We handle exclude lists below. We always want to print the root dir
+    # no matter what.
     yield f'{indent}{os.path.basename(root)}/'
 
     indent += INDENT_STEP
     for entry in sorted(os.scandir(root), key=lambda e: e.name):
-        line = f'{indent}{entry.name}'
-
-        if entry.is_dir():
-            yield from _tree(entry.path, indent=indent)
-        else:
-            yield line
+        if entry.is_dir() and entry.name not in exclude_dirs:
+            yield from _tree(entry.path, exclude_dirs, indent=indent)
+        elif not entry.is_dir():
+            yield f'{indent}{entry.name}'
 
 
 if __name__ == '__main__':
-    try:
-        root = sys.argv[1]
-    except IndexError:
-        root = '.'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('root', nargs='?', default='.',
+                        help='directory root')
+    parser.add_argument('--exclude', action='append',
+                        help='directories to skip')
+    args = parser.parse_args()
+
+    root = args.root
 
     # Handle . and ./ gracefully
     if root in ('.', './'):
@@ -35,4 +42,5 @@ if __name__ == '__main__':
     else:
         root = os.path.abspath(root)
 
-    print_tree(root)
+    print(args.exclude)
+    print_tree(root, exclude_dirs=args.exclude)
