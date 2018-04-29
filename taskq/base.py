71e1204fab14c3d0dcc21d7a4a0cf1973d1dde22
Francesco Pierfederici <fpierfed@megabeets.com>
2018-04-29 11:57:28 +0200

    initial

---
+++ taskq/base.py
@@ -0,0 +1,16 @@
+def fib(n):
+    if n <= 2:
+        return 1
+    elif n == 0:
+        return 0
+    elif n < 0:
+        raise Exception('fib(n) is undefined for n < 0')
+    return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    N = 35
+    REPETITIONS = 4
+
+    for _ in range(REPETITIONS):
+        res = fib(N)
