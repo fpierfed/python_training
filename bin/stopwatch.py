167a104e232403ed4be2937e518a1a0dfdfad5b0
Francesco Pierfederici <fpierfed@megabeets.com>
2019-02-17 18:03:33 +0100

    better time representation

--- bin/stopwatch.py
+++ bin/stopwatch.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+from datetime import datetime
 import shutil
 import sys
 import time
 
 
 w, _ = shutil.get_terminal_size()
-t0 = time.time()
+t0 = datetime.now()
 try:
     while True:
-        print(f'{time.time() - t0:>{w}.01f}', end='\r', file=sys.stdout)
+        s, ms = str(datetime.now() - t0).rsplit('.', 1)
+        s = f'{s}.{ms[0]}'
+        print(f'{s:>{w}}', end='\r', file=sys.stdout)
         sys.stdout.flush()
         time.sleep(.1)
 except KeyboardInterrupt:
