04e67e323d8381b3cb232b6fd8fa2b657730eaf7
Francesco Pierfederici <fpierfed@megabeets.com>
2019-03-03 17:26:07 +0100

    slighly more advanced

--- async/code/06-pipe/pipe_asyncio.py
+++ async/code/06-pipe/pipe_asyncio.py
@@ -1,8 +1,14 @@
 # pipe_asyncio.py
 import subprocess
 import time
-
 from asyncio import get_event_loop, wait, sleep
+from asyncio import Task as AsyncioTask
+
+
+class Task(AsyncioTask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = len(Task.all_tasks())
 
 
 async def runner(argv, timeout=0):
@@ -33,15 +39,16 @@ def logging_cb(task):
     t = time.time()
 
     if task.exception():
-        print(f'Task {id(task)} raised {task.exception()!r} at t={t}')
+        print(f'Task {task.id} raised {task.exception()!r} at t={t}')
     else:
         print(
-            f'Task {id(task)} exited with status code {task.result()} at t={t}'
+            f'Task {task.id} exited with status code {task.result()} at t={t}'
         )
 
 
 if __name__ == '__main__':
     loop = get_event_loop()
+    loop.set_task_factory(lambda loop, coro: Task(coro))
 
     tasks = [
         loop.create_task(runner(['sleep', 5])),
